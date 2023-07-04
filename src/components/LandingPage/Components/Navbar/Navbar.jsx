a8b028717a19fb1d59709444fbcb132a010c9a14
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2023-07-04 14:04:39 +0530

    added styles i navbar

--- src/components/LandingPage/Components/Navbar/Navbar.jsx
+++ src/components/LandingPage/Components/Navbar/Navbar.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import "./Navbar.css";
 
 const Navbar = () => {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    const Close = () => setClick(false);   
+
   return (
     <div>
-        Navbar
-    </div>
-  )
-}
+        <div className={click ? "main-container" : ""}  onClick={()=>Close()} />
+        <nav className="navbar" onClick={e => e.stopPropagation()}>
+            <div className="nav-container">
+                <a href="/" className="nav-logo">
+                    StayHealthy <i className="fa fa-user-md"></i>
+                </a>
+                <ul className={click ? "nav-menu active" : "nav-menu"}>
+                    <li className="nav-item">
+                        Home
+                    </li>
+                    <li className="nav-item">
+                        About
+                    </li>
+                    <li className="nav-item">
+                        Contact
+                    </li>
+                    <li className="nav-item">
+                        Appointment
+                    </li>
+                    <li className="nav-item">
+                        Login/Signup <i className="fa fa-user"></i>
+                    </li>
+                    {/* Add search field as an input with search icon */}
+                    <li className="nav-item">
+                        <i className="fa fa-search"></i>
+                    </li>
+                    
+                </ul>
+                <div className="nav-icon" onClick={handleClick}>
+                    <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
+                </div>
+            </div>
+        </nav>
+    </ div>
+  );
+};
 
-export default Navbar
+export default Navbar;
