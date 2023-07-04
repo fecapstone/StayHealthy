3af580809e2213b6c5b800f5ac1b8478b35ca741
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2023-07-05 00:47:10 +0530

    Created Layout for header and footer to be reused

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import { Navbar } from './components/LandingPage/Components';
+import Home from './components/LandingPage/Pages/Home';
+import Layout from './components/LandingPage/Components/Layout/Layout';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Router>
-        <Routes>
-          <Route path="/" element={<h1>Home</h1>}/>
-          <Route path="/about" element={<h1>About</h1>}/>
+        <Router>
+          <Layout>
+              <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<h1>About</h1>}/>
 
-        </Routes>
-      </Router>
-      {/* routes */}
+              </Routes>
+          </Layout>
+        </Router>
     </div>
   );
 }
