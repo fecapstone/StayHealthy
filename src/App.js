f11d323d8c1f48cbd7339a3d71239b10fe7ad5c9
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2023-07-04 12:08:54 +0530

    Created sample routes and added navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { Navbar } from './components/LandingPage/Components';
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <Navbar/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>}/>
+          <Route path="/about" element={<h1>About</h1>}/>
+
+        </Routes>
+      </Router>
       {/* routes */}
     </div>
   );
