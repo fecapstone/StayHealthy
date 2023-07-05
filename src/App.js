e285502a941224e5c8af12907f1d186ad540e373
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2023-07-05 15:43:16 +0530

    added AOS for scroll animation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-
+import React, { useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Home from './components/LandingPage/Pages/Home';
 import Layout from './components/LandingPage/Components/Layout/Layout';
+import AOS from 'aos';
+import { Home, Login, SignUp } from './components';
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      once: false,
+      duration: 3000,
+    })
+  }, [])
   return (
     <div className="App">
         <Router>
           <Layout>
               <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/about" element={<h1>About</h1>}/>
+                <Route path="/" element={<Home />}/>
+                <Route path="/login" element={<Login />}/>
+                <Route path="/signup" element={<SignUp />}/>
+
 
               </Routes>
           </Layout>
