4379ae8d1b81bb520814620cbb3e1255727635ea
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2023-07-06 18:14:08 +0530

    Exported Book Appointment component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Home } from './LandingPage/Pages/Home';
 export { default as Login } from './Login/Login';
 export { default as SignUp } from './SignUp/SignUp';
+export { default as BookAppointments } from './BookAppointments/BookAppointments';
