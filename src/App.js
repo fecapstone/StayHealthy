c1445b552108c07cffeee6a7024f17d446ed2211
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2023-07-10 18:57:24 +0530

    Added necessary routes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Layout from './components/LandingPage/Components/Layout/Layout';
 import AOS from 'aos';
-import { BookAppointments, Home, Login, SignUp } from './components';
+import { BookAppointments, GiveReviews, Home, InstantConsultation, Login, Profile, Reports, SignUp } from './components';
 import { Reviews } from './components/LandingPage/Components';
 import HealthTips from './components/HeathTips/HealthTips';
 import Setauthtoken from './Setauthtoken';
@@ -22,11 +22,14 @@ function App() {
           <Layout>
               <Routes>
                 <Route path="/" element={<Home />}/>
-                <Route path="/reviews" element={<Reviews />}/>
+                <Route path="/reviews" element={<GiveReviews />}/>
                 <Route path="/healthblog" element={<HealthTips />}/>
                 <Route path="/login" element={<Login />}/>
                 <Route path="/signup" element={<SignUp />}/>
                 <Route path="/search/doctors" element={<BookAppointments />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/reports" element={<Reports />} />
+                {/* <Route path="/instant-consultation" element={<InstantConsultation />} /> */}
 
                 <Route
                   path="/setauthtoken/:authtoken"
