7f0859245bd4c978adb3579ebb1d5db6affe7235
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2023-07-28 12:19:40 +0530

    changes made

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -89,10 +89,53 @@ router.post('/register',[
 });
 
 // Route 2: Authenticating an existing user: POST: http://localhost:8181/api/auth/login. No Login Required
+// router.post('/login', [
+//     body('name', "Username should be at least 4 characters.").isLength({ min: 4 }),
+//     body('password', "Password Should Be At Least 8 Characters.").isLength({ min: 8 }),
+// ], async (req, res) => {
+
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//         return res.status(400).json({ errors: errors.array() });
+//     }
+
+
+//     try {
+//         const theUser = await UserSchema.findOne({ username: req.body.username });
+//         // store the username so that we can show the username on the screen
+//         req.session.username = req.body.name;
+//         console.log(req.session.username);
+
+
+//         if (theUser) {
+//             // console.log(checkEmailExists);
+//             let checkHash = await bcrypt.compare(req.body.password, theUser.password);
+//             if (checkHash) {
+//                 let payload = {
+//                     user: {
+//                         id: theUser.id
+//                     }
+//                 }
+//                 const authtoken = jwt.sign(payload, JWT_SECRET);
+//                 return res.status(200).json({authtoken});
+//             }
+//             else {
+//                 return res.status(403).json({ error: "Invalid Credentials" });
+//             }
+//         }
+//         else {
+//             return res.status(403).json({ error: "Invalid Credentials" });
+//         }
+        
+        
+//     } catch (error) {
+//         console.error(error);
+//         return res.status(500).send("Internal Server Error");
+//     }
+// });
+
 router.post('/login', [
     body('name', "Username should be at least 4 characters.").isLength({ min: 4 }),
-    // body('email', "Please Enter a Vaild Email").isEmail(),
-    // body('phone', "Phone Number Should Be 10 Digits.").isLength({ min: 10 }),
     body('password', "Password Should Be At Least 8 Characters.").isLength({ min: 8 }),
 ], async (req, res) => {
 
@@ -101,16 +144,13 @@ router.post('/login', [
         return res.status(400).json({ errors: errors.array() });
     }
 
-
     try {
-        const theUser = await UserSchema.findOne({ username: req.body.username });
+        const theUser = await UserSchema.findOne({ username: req.body.name }); // <-- Change req.body.username to req.body.name
         // store the username so that we can show the username on the screen
-        req.session.username = req.body.username;
+        req.session.username = req.body.name; // <-- Change req.body.username to req.body.name
         console.log(req.session.username);
 
-
         if (theUser) {
-            // console.log(checkEmailExists);
             let checkHash = await bcrypt.compare(req.body.password, theUser.password);
             if (checkHash) {
                 let payload = {
@@ -119,23 +159,19 @@ router.post('/login', [
                     }
                 }
                 const authtoken = jwt.sign(payload, JWT_SECRET);
-                return res.status(200).json({authtoken});
-            }
-            else {
+                return res.status(200).json({ authtoken });
+            } else {
                 return res.status(403).json({ error: "Invalid Credentials" });
             }
-        }
-        else {
+        } else {
             return res.status(403).json({ error: "Invalid Credentials" });
         }
-        
-        
+
     } catch (error) {
         console.error(error);
         return res.status(500).send("Internal Server Error");
     }
 });
-
 
 // Route 3: Updating existing specific User details login required
 // router.put('/update', [
